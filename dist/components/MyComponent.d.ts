cb4b86cb2efbabb718be1695e7ad1fcb8e4be705
lambou <lambouarnold@gmail.com>
2026-01-12 20:13:45 +0100

    dist folder removed from the repo

--- dist/components/MyComponent.d.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import { MyComponentProps } from "../types";
-export declare const MyComponent: React.FC<MyComponentProps>;
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { MyComponent } from './components';
-export type { MyComponentProps } from './types';
